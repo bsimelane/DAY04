750e5640ff709ef6727cf29d2fd2ebe31a0bf07c
Bongani Christopher Simelane <bsimelan@c5r1s9.wethinkcode.co.za>
2020-06-26 08:57:38 +0200

    ex02

--- ex02/ft_iterative_power.c
+++ ex02/ft_iterative_power.c
@@ -6,23 +6,23 @@
 /*   By: bsimelan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/25 13:43:02 by bsimelan          #+#    #+#             */
-/*   Updated: 2020/06/25 17:11:02 by bsimelan         ###   ########.fr       */
+/*   Updated: 2020/06/26 08:56:56 by bsimelan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_iterative_power(int nb, pow)
+int ft_iterative_power(int nb, int power)
 {
 	int a;
-	pow = nb;
+	power = nb;
 	a = 1;
 
-	if (pow == 0 || pow == 1 || nb == 0 || nb == 1)
+	if (power == 0 || power == 1 || nb == 0 || nb == 1)
 		return (1);
 
-	while(pow > 0)
+	while(power > 0)
 	{
 		a  = (a * nb);
-		pow--;
+		power--;
 	}
 	return (a);
 }
